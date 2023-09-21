9327c70fe81aa4baf7d5919915264e94898a6b11
Gianni Narracci <gnarracci@gmail.com>
2023-09-21 18:47:22 -0400

    Adding new styles to menu

--- src/app/components/sidenav/nav-data.ts
+++ src/app/components/sidenav/nav-data.ts
@@ -1,7 +1,10 @@
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
+
 export const navbarData = [
+
   {
     routerLink: 'dashboard',
-    icon: 'fas fa-home',
     label: 'Dashboard'
   }
+
 ]
--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { navbarData } from './nav-data';
-import {faTimes} from '@fortawesome/free-solid-svg-icons'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-sidenav',
